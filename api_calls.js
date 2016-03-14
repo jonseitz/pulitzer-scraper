39e5018c8a5f592d89e3e39b509fc43cf1c48de3
Jonathan Seitz <seitz.jon@gmail.com>
2016-03-13 23:41:23 -0400

    commenting for the people

--- api_calls.js
+++ api_calls.js
@@ -2,36 +2,41 @@ var request = require('request');
 var assert = require('assert');
 
 var API_CALLS = {
+	//Grab all of the winners in a single category, returning an array of simplified objects.
 	get_winners_by_cat: function (tid, callback) {
+		//checks that tid is a valid category
 		if (tid >= 204 && tid <= 225) {
 			this.get_cat_from_tid(tid, function(tid, catName){
-					var winnersUrl = 'http://www.pulitzer.org/cms/api/1/winners/cat/' + tid + '/raw.json';
-					request({
-						url: winnersUrl,
-						json: true
-					}, function(error, response, body){
-						assert.equal(error, null);
-						var list = [];
-						if (body) {
-							body.forEach(function(val, index){
-								var year = 2015 - (val.field_year.und[0].tid - 105);
-								var publication, citation = 'N/A';
-								if (val.field_publication.und){
-									publication = val.field_publication.und[0].safe_value;
-								}
-								if (val.field_abbr_citation.und){
-									citation = val.field_abbr_citation.und[0].safe_value;
-								}				
-								list.push({
-									"category": catName,
-									"year": year,
-									"title": val.title,
-									"publication": publication,
-									"citation": citation
-								});
+				//forming the API endpoint url
+				var winnersUrl = 'http://www.pulitzer.org/cms/api/1/winners/cat/' + tid + '/raw.json';
+				request({
+					url: winnersUrl,
+					json: true
+				}, function(error, response, body){
+					assert.equal(error, null);
+					var list = [];
+					if (body) {
+						body.forEach(function(val, index){
+							//compute the year from tid
+							var year = 2015 - (val.field_year.und[0].tid - 105);
+							// get the citation and publication info, or return "N/A"
+							var publication, citation = 'N/A';
+							if (val.field_publication.und){
+								publication = val.field_publication.und[0].safe_value;
+							}
+							if (val.field_abbr_citation.und){
+								citation = val.field_abbr_citation.und[0].safe_value;
+							}				
+							list.push({
+								"category": catName,
+								"year": year,
+								"title": val.title,
+								"publication": publication,
+								"citation": citation
 							});
-							callback(list);
-						}
+						});
+						callback(list);
+					}
 				});
 			});
 		}
@@ -40,6 +45,7 @@ var API_CALLS = {
 		}
 	},
 	get_cat_from_tid: function (tid, callback) {
+			//forming the API url
 			var catUrl = 'http://www.pulitzer.org/cms/api/1/term/' + tid + '/raw.json';
 			request({
 				url: catUrl,
@@ -53,15 +59,6 @@ var API_CALLS = {
 					return null
 				}
 			});
-	},
-	get_year_from_tid: function(tid){
-		if (tid < 105 || tid > 203){
-			return null;
-		}
-		else {
-			var diff = tid - 105;
-			return (2015 - diff);
-		}
 	}
 };
 
--- app.js
+++ app.js
@@ -7,17 +7,22 @@ var API = require('./api_calls');
 
 var fields = ['category', 'year', 'title', 'publication', 'citation'];
 var allWinners = [];
+
+//setting initial tid value to 204 (lowest category value)
 var tid = 204;
+
+//loop through tid values below 225 (highest category value)
 while(tid >= 204 && tid <= 225){
 	API.get_winners_by_cat(tid, function(list){
 		json2csv({data: list, fields: fields}, function(err, csv){
 			if (err) console.log(err);
+			//output to 'winners.csv' file
 			fs.appendFile('winners.csv', csv, function (err){
 				if(err) throw err;
 			});
 		});
 
 	});
-	tid += 1;
 	console.log('category ' + tid + ' saved');	
+	tid += 1;
 }
