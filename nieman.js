f30528b4990e853fa23fd03af8583185ad95280d
Jonathan Seitz <seitz.jon@gmail.com>
2016-03-24 08:24:36 -0400

    removing debugging comments

--- nieman.js
+++ nieman.js
@@ -30,5 +30,28 @@ fs.createReadStream('nieman-fellows.csv')
 						});
 					}
 				}
+			});
+			fs.createReadStream('finalists.csv')
+			.pipe(csv())
+			.on('data', function(prize){
+				for (var field in prize){
+					//checks to see if last name and either given or perferred name matches any fields in the prize list
+					if (prize[field].search(fellow.LAST) != -1 && prize[field].search(fellow.FIRST) != -1) {
+						prize.fellow = fellow['FULL NAME'];
+						prize.NF = fellow.YEAR;
+						json2csv({data: prize}, function(err, csv){
+							if (err) {
+								console.log(err);
+							}
+							csv = csv.replace('"category","year","type","title","publication","citation","fellow","NF"', '');
+							csv = csv.replace('\\n', '');
+							console.log(csv);
+							fs.appendFile('nieman-finalists.csv', csv, function (err){
+								console.log('Found finalist: ' + fellow['FULL NAME']);
+								if(err) throw err;
+							});
+						});
+					}
+				}
 			});	
 		});
