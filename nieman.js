d8244dfa69e1ad657eb1abf5e2cfc665e0afa57b
Jonathan Seitz <seitz.jon@gmail.com>
2016-03-23 23:37:21 -0400

    separate file to read list of nieman fellows and compare against pulitzer winners

---
+++ nieman.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+var assert = require('assert');
+var csv = require('csv-parser');
+var json2csv = require('json2csv');
+
+var list = [];
+
+fs.createReadStream('nieman-fellows.csv')
+	.pipe(csv())
+	.on('data', function(fellow){
+		fs.createReadStream('winners.csv')
+			.pipe(csv())
+			.on('data', function(prize){
+				for (var field in prize){
+					//checks to see if last name and either given or perferred name matches any fields in the prize list
+					if (prize[field].search(fellow.LAST) != -1 && prize[field].search(fellow.FIRST) != -1) {
+						prize.fellow = fellow['FULL NAME'];
+						prize.NF = fellow.YEAR;
+						json2csv({data: prize}, function(err, csv){
+							if (err) {
+								console.log(err);
+							}
+							csv.replace('"category","year","type","title","publication","citation","fellow","NF"', '');
+							fs.appendFile('nieman-winners.csv', csv, function (err){
+								console.log('Found winner: ' + fellow['FULL NAME']);
+								if(err) throw err;
+							});
+						});
+					}
+				}
+			});	
+		});
